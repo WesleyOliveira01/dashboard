60ba1b785345c5cb284f783f673b4b9de5c48243
wesleyoliveira01 <wesleyoliveira.dev@outlook.com>
2023-12-20 16:51:28 -0300

    feat: Modified signup form to add user hierarchy

--- components/login/SignUp-Form.tsx
+++ components/login/SignUp-Form.tsx
@@ -7,10 +7,11 @@ import {
   CardHeader,
   CardTitle,
 } from "../ui/card";
+import { RadioGroup, RadioGroupItem } from "../ui/radio-group";
 
 const SignUpForm = () => {
   return (
-    <Card className="w-[350px]">
+    <Card className="lg:w-[50%] w-[95%]">
       <CardHeader className="items-center justify-center">
         <CardTitle>Sign-up title here</CardTitle>
       </CardHeader>
@@ -18,18 +19,27 @@ const SignUpForm = () => {
         <form>
           <div className="grid w-full items-center gap-4">
             <div className="flex flex-col space-y-1.5">
-              <Input
-                label="Nome"
-                forElement="nome"
-              />
+              <Input label="Nome" forElement="nome" />
             </div>
             <div className="flex flex-col space-y-1.5">
               <Input
                 type="email"
                 label="E-mail"
                 forElement="email"
-                placeholder="enter your email"
               />
+            </div>
+            <div className="flex flex-col space-y-1.5">
+              <h1 className="font-semibold">Permissões do usuario</h1>
+              <RadioGroup className="lg:flex lg:flex-row lg:justify-around lg:w-[50%]">
+                <div className="flex items-center space-x-2">
+                  <RadioGroupItem value="default-user" id="default-user" />
+                  <label htmlFor="default-user">Usuario padrão</label>
+                </div>
+                <div className="flex items-center space-x-2">
+                  <RadioGroupItem value="admin" id="admin" />
+                  <label htmlFor="admin">Administrador</label>
+                </div>
+              </RadioGroup>
             </div>
             <div className="flex flex-col space-y-1.5">
               <Input type="password" forElement="password" label="Senha" />
