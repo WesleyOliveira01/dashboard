d8680621bfa73c16daa490d22513d02e4b0847ef
wesleyoliveira01 <wesleyoliveira.dev@gmail.com>
2023-12-20 21:51:58 -0300

    feat: concepts at plans component card

--- app/dashboard/plans/page.tsx
+++ app/dashboard/plans/page.tsx
@@ -1,11 +1,26 @@
+import Button from "@/components/ui/Button";
 import Container from "@/components/ui/Container";
+import { BadgeX } from "lucide-react";
 
 const Plans = () => {
-    return ( 
-        <Container>
-            planos
-        </Container>
-     );
-}
- 
+  return (
+    <Container>
+      <ul>
+        <li>
+          <section className="shadow-md p-2 rounded-md flex items-center gap-1">
+            <ul className="flex justify-evenly text-sm">
+              <li> Nome do plano</li>
+              <li> descricao do plano</li>
+              <li> valor do plano</li>
+            </ul>
+            <Button>
+              <BadgeX size={20} color="red" />
+            </Button>
+          </section>
+        </li>
+      </ul>
+    </Container>
+  );
+};
+
 export default Plans;
