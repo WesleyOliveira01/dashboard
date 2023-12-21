6c304390ccc7276e2e4e22f457eec9f81f6b25bf
wesleyoliveira01 <wesleyoliveira.dev@gmail.com>
2023-12-20 21:53:53 -0300

    feat: create user and user signUpData interfaces

---
+++ interfaces/auth-interfaces.ts
@@ -0,0 +1,12 @@
+interface authData{
+    email:string
+    password:string
+}
+
+interface signUpData{
+    name:string,
+    email:string,
+    password:string
+}
+
+export type { authData, signUpData }
