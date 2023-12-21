1596131cefbfaae9447368f51afeb62365636f51
wesleyoliveira01 <wesleyoliveira.dev@gmail.com>
2023-12-20 21:52:25 -0300

    feat:concepts at clients components

--- app/dashboard/clients/page.tsx
+++ app/dashboard/clients/page.tsx
@@ -1,11 +1,21 @@
 import Container from "@/components/ui/Container";
 
 const Clients = () => {
-    return ( 
-        <Container>
-            clients
-        </Container>
-     );
-}
- 
+  return (
+    <Container>
+      <ul>
+        <li>
+          <section className="shadow-md p-2 rounded-md flex items-center gap-1">
+            <ul className="flex justify-evenly text-sm">
+              <li> Nome do cliente</li>
+              <li> plano escolhido</li>
+              <li> endereco</li>
+            </ul>
+          </section>
+        </li>
+      </ul>
+    </Container>
+  );
+};
+
 export default Clients;
