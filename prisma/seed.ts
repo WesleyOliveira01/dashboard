50801ccc800aac23894f5a2d30f93652ef3c3757
wesleyoliveira01 <wesleyoliveira.dev@gmail.com>
2023-12-24 14:02:50 -0300

    refactor: modifing admin name

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,7 +3,7 @@ import prisma from "@/lib/db";
 async function createAdmin() {
     await prisma.user.create({
         data: {
-            name: "admin",
+            name: "Wesley Alves",
             email: "wesleyoliveira.dev01@gmail.com",
             password: "$2b$10$tjNCtySA62c3Z2QaWzpu7.sucmGLaXjaiK.BdNd6kIuLz8jvxGCUG",
             isAdmin: true
