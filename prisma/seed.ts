c5e16ec76f641dce5a54d6cfb0719de3f515c519
wesleyoliveira01 <wesleyoliveira.dev@outlook.com>
2023-12-28 16:39:03 -0300

    refactor: moddifieing to better generate user admin at start project

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,20 +1,19 @@
 import prisma from "@/lib/db";
 
 export async function createAdmin() {
-    const id = "$2b$10$tjNCtySA62c3Z2QaWzpu7.sucmGLaXjaiK.BdNd6kIuLz8jvxGCUG"
+    const name = "Wesley Alves"
 
-    const hasUser = await prisma.user.findFirst({where:{id,}})
-    if(!!hasUser){
-        return
+    const hasUser = await prisma.user.findFirst({where:{name,}})
+    if(!hasUser){
+        await prisma.user.create({
+            data: {
+                name: "Wesley Alves",
+                email: "wesleyoliveira.dev01@gmail.com",
+                password: "$2b$10$tjNCtySA62c3Z2QaWzpu7.sucmGLaXjaiK.BdNd6kIuLz8jvxGCUG",
+                isAdmin: true
+            }
+        })
     }
-
-    await prisma.user.create({
-        data: {
-            name: "Wesley Alves",
-            email: "wesleyoliveira.dev01@gmail.com",
-            password: "$2b$10$tjNCtySA62c3Z2QaWzpu7.sucmGLaXjaiK.BdNd6kIuLz8jvxGCUG",
-            isAdmin: true
-        }
-    })
+    return 
 }
 
