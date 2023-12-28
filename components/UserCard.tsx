b2b1bb9e24e3d16c20e758e173e12c7b1da176f2
wesleyoliveira01 <wesleyoliveira.dev@outlook.com>
2023-12-28 16:42:27 -0300

    refactor: updanting usercard componet to active pop up componet at delete user

--- components/UserCard.tsx
+++ components/UserCard.tsx
@@ -1,27 +1,44 @@
-"use client"
+"use client";
+import * as authService from "@/actions/auth/authservice";
 import { IUserCard } from "@/interfaces/auth-interfaces";
 import { BadgeX } from "lucide-react";
 import Link from "next/link";
+
+import { useState } from "react";
 import Button from "./ui/Button";
-import * as authService from "@/actions/auth/authservice";
+import PopUp from "./ui/PopUp";
 
 const UserCard = ({ name, email, permission, id }: IUserCard) => {
-  const {deleteUser} = authService
+  const { deleteUser } = authService;
+  const [error, setError] = useState<string | null>(null);
+  const removeUser = async (id: string) => {
+    try {
+      await deleteUser(id);
+    } catch (error) {
+      setError(error.message);
+      setTimeout(() => {
+        setError(null);
+      }, 3000);
+    }
+  };
   return (
-    <li className="w-full">
-      <section className="shadow-md p-2 rounded-md flex justify-evenly items-center">
-        <Link href={`/dashboard/users/${id}`} as={`/dashboard/users/${id}`}>
-          <ul className="flex justify-evenly gap-5 font-semibold">
-            <li>{name}</li>
-            <li>{email}</li>
-            <li>{permission ? "administrador" : "Usuario padrao"}</li>
-          </ul>
-        </Link>
-        <Button onClick={async () => await deleteUser(id)}>
-          <BadgeX size={20} color="red" />
-        </Button>
-      </section>
-    </li>
+    <>
+      <li className="w-full">
+        <section className="shadow-md p-2 rounded-md flex justify-between items-center">
+          <Link href={`/dashboard/users/${id}`}>
+            <ul className="flex justify-between  gap-5 px-4 font-semibold">
+              <li>{name}</li>
+              <li>{email}</li>
+              <li>{permission ? "administrador" : "Usuario padrao"}</li>
+            </ul>
+          </Link>
+          <Button  onClick={async () => await removeUser(id)}>
+            <BadgeX size={20} color="red" />
+          </Button>
+        </section>
+      </li>
+      {error && <PopUp isError={true} message={error} />}
+    </>
   );
 };
 
