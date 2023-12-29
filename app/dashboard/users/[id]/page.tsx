e0f7fef078aeb28d080ee5bfc6569985425394a7
wesleyoliveira01 <wesleyoliveira.dev@gmail.com>
2023-12-29 11:59:06 -0300

    style: remove death code

--- app/dashboard/users/[id]/page.tsx
+++ app/dashboard/users/[id]/page.tsx
@@ -1,8 +1,9 @@
 import Container from '@/components/ui/Container';
-import { NextRequest } from 'next/server';
+
 
 const UserDetails = async ({params}:{params:{id:string}}) => {
     const id = params.id;
+
     return ( 
         <Container>
             <p>id do usuario: {id}</p>
