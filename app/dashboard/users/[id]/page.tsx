50a887a2a697ab1439677204b1bd6824d011cb15
wesleyoliveira01 <wesleyoliveira.dev@outlook.com>
2023-12-31 20:55:24 -0300

    feat: begin add aditional styles to users dashboard

--- app/dashboard/users/[id]/page.tsx
+++ app/dashboard/users/[id]/page.tsx
@@ -1,13 +1,29 @@
-import Container from '@/components/ui/Container';
-import { NextRequest } from 'next/server';
+import * as authService from "@/actions/auth/authservice";
+import Container from "@/components/ui/Container";
+import Button from '@/components/ui/Button'
 
-const UserDetails = async ({params}:{params:{id:string}}) => {
-    const id = params.id;
-    return ( 
-        <Container>
-            <p>id do usuario: {id}</p>
-        </Container>
-     );
-}
- 
+const UserDetails = async ({ params }: { params: { id: string } }) => {
+  const { getUserByID } = authService;
+  const id = params.id;
+  const user = await getUserByID(id);
+  return (
+    <Container>
+        <section className="w-full flex items-center justify-between p-4">
+        <h1 className="font-semibold text-2xl text-zinc-950">
+          Dados do usuarios
+        </h1>
+
+        <Button>
+            Atualizar dados
+        </Button>
+      </section>
+      <ul className="w-[70%] shadow-md p-4 rounded-md [&>*]:p-2 [&>*]:flex [&>*]:gap-2">
+        <li><h2 className="font-semibold">Nome:</h2> {user?.name}</li>
+        <li><h2 className="font-semibold">E-mail:</h2> {user?.email}</li>
+        <li><h2 className="font-semibold">Permissão:</h2> {user?.isAdmin ? "Admistrador" : "Usuario padrão"}</li>
+      </ul>
+    </Container>
+  );
+};
+
 export default UserDetails;
--- app/dashboard/users/page.tsx
+++ app/dashboard/users/page.tsx
@@ -1,16 +1,25 @@
 import * as authService from "@/actions/auth/authservice";
 import UserCard from "@/components/UserCard";
+import { UserRoundPlus } from "lucide-react";
+import Link from "next/link";
 
 const Users = async () => {
   const { getAllUsers } = authService;
   const users = await getAllUsers();
   return (
     <main className="flex flex-col items-center justify-center p-2">
-      <h1 className="font-semibold text-xl text-zinc-950">
-        Painel de usuarios
-      </h1>
+      <section className="w-full flex items-center justify-between p-4">
+        <h1 className="font-semibold text-2xl text-zinc-950">
+          Painel de usuarios
+        </h1>
 
-      <ul className="flex flex-col gap-2 lg:w-[45%] w-[90%]">
+        <Link href="/dashboard/signUp" className="flex font-semibold gap-2">
+          Cadastrar novo usuario
+          <UserRoundPlus size={20} color="#09090B" />
+        </Link>
+      </section>
+
+      <ul className="flex flex-col gap-2 lg:w-[50%] w-[90%]">
         {users.map(({ id, name, email, isAdmin }) => (
           <UserCard
             key={id}
