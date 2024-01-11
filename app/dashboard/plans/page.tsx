1f301f66eaf8196ef9193e00763712a5b859a339
wesleyoliveira01 <wesleyoliveira.dev@outlook.com>
2024-01-11 13:48:14 -0300

    feat: rendering all plans at the dashboard

--- app/dashboard/plans/page.tsx
+++ app/dashboard/plans/page.tsx
@@ -1,24 +1,25 @@
-import Button from "@/components/ui/Button";
-import Container from "@/components/ui/Container";
-import { BadgeX } from "lucide-react";
+import * as planService from "@/actions/plans/planService";
 
-const Plans = () => {
+import Container from "@/components/ui/Container";
+
+import RenderPlans from "@/components/RenderPlans";
+import { Plan } from "@prisma/client";
+import Link from "next/link";
+import Loading from "../loading";
+import { BadgePlus } from "lucide-react";
+
+const Plans = async () => {
+  const { getAllPlans } = planService;
+  const plans: Plan[] = await getAllPlans();
   return (
     <Container>
-      <ul>
-        <li>
-          <section className="shadow-md p-2 rounded-md flex items-center gap-1">
-            <ul className="flex justify-evenly text-sm">
-              <li> Nome do plano</li>
-              <li> descricao do plano</li>
-              <li> valor do plano</li>
-            </ul>
-            <Button>
-              <BadgeX size={20} color="red" />
-            </Button>
-          </section>
-        </li>
-      </ul>
+      <section className="p-3 font-semibold flex justify-between w-full">
+        <h1 className="text-xl">Planos cadastrados</h1>
+        <h1 className="cursor-pointer">
+          <Link href="/dashboard/plans/newPlan"><span className="mobile:hidden">Cadastrar novo plano</span> <BadgePlus color="green" /></Link>
+        </h1>
+      </section>
+      {plans.length == 0 ? <Loading /> : <RenderPlans plans={plans} />}
     </Container>
   );
 };
---
+++ components/RenderPlans.tsx
@@ -0,0 +1,74 @@
+"use client";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+import Button from "@/components/ui/Button";
+import { Plan } from "@prisma/client";
+import { BadgeX, PencilRuler } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useToast } from "./ui/use-toast";
+
+import * as planService from "@/actions/plans/planService";
+const RenderPlans = ({ plans }: { plans: Plan[] }) => {
+  const { deletePlan } = planService;
+  const { toast } = useToast();
+  const router = useRouter();
+  const removePlan = async (id: string) => {
+    try {
+      await deletePlan(id);
+      toast({
+        description: "Plano removido com sucesso",
+      });
+      router.refresh();
+    } catch (e) {
+      toast({
+        description: e.message,
+        variant: "destructive",
+      });
+    }
+  };
+  return (
+    <section className=" w-[90%] ">
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead><span className="mobile:hidden">Com</span> fidelidade</TableHead>
+            <TableHead>Nome</TableHead>
+            <TableHead>Valor</TableHead>
+            <TableHead className="mobile:hidden">Descrição</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {plans.map((plan) => (
+            <TableRow key={plan.id}>
+              <TableCell>{plan.fidelity ? "Sim" : "Não"}</TableCell>
+              <TableCell>{plan.name}</TableCell>
+              <TableCell>{plan.price}</TableCell>
+              <TableCell className="mobile:hidden">{plan.description}</TableCell>
+              <TableCell>
+                <Button>
+                  <PencilRuler color="green" />
+                </Button>{" "}
+                <Button
+                  onClick={async () => {
+                    await removePlan(plan.id);
+                  }}
+                >
+                  <BadgeX color="red" />
+                </Button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </section>
+  );
+};
+
+export default RenderPlans;
