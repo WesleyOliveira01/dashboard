022a4a95ab0d28ccbfee7c7ba0bb8e282df9a877
wesleyoliveira01 <wesleyoliveira.dev@outlook.com>
2024-01-18 16:58:22 -0300

    feat: creating function for formating data

--- actions/utils.ts
+++ actions/utils.ts
@@ -3,9 +3,21 @@ import { verify } from "jsonwebtoken";
 import { cookies } from "next/headers";
 import { IToken, TokenPayload } from "@/interfaces/auth-interfaces";
 
-export async function verifyToken() {
+async function verifyToken() {
   const { value: cookie } = cookies().get("token") as any as IToken;
   const token = verify(cookie, process.env.JWT_SECRET) as TokenPayload;
 
   if (!token.isAdmin) throw new Error("Usuario sem permissão");
 }
+
+
+function formatarData(data) {
+  if(!data) return "";
+  const dia = data.slice(0, 2);
+  const mes = data.slice(2, 4);
+  const ano = data.slice(4);
+  return `${dia}/${mes}/${ano}`;
+}
+
+
+export {verifyToken, formatarData};
