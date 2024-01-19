10805cb2e36842087dedaa1b0bedb95565b3be97
wesleyoliveira01 <wesleyoliveira.dev@outlook.com>
2024-01-19 16:43:01 -0300

    feat: creating get all clients service

--- actions/client/clientService.ts
+++ actions/client/clientService.ts
@@ -1,29 +1,48 @@
 "use server";
 
+import prisma from "@/lib/db";
 import { redirect } from "next/navigation";
 import { formatarData } from "../utils";
-import prisma from "@/lib/db";
 
 async function createClient(data: any) {
   const endereco = `${data.endereco},${data.numero} ${data.complemento}, ${data.cep}`;
   const dataDeNascimento = formatarData(data.nascimento);
   const client = {
     nome: data.nome as string,
-    cpf_cnpj: Number(data.cpf),
-    rg_ie: Number(data.rg),
+    cpf_cnpj: data.cpf,
+    rg_ie: data.rg,
     email: data.email.toLowerCase() as string,
-    telefone: Number(data.telefone),
+    telefone: data.telefone,
     nascimento: dataDeNascimento,
     endereco: endereco,
-    plan_id:data.plano as string
+    plan_id: data.plano as string,
+    vencimento: data.vencimento,
   };
   await prisma.client.create({
     data: {
-      ...client
+      ...client,
     },
   });
 
   redirect("/dashboard/clients");
 }
 
-export {createClient}
+async function getClients() {
+  let allClients = await prisma.client.findMany({
+    select: {
+      nome: true,
+      telefone: true,
+      email: true,
+      endereco: true,
+      vencimento: true,
+      id: true,
+      plan: {
+        select: { name: true, description: true, price: true, fidelity: true },
+      },
+    },
+  });
+  
+  return allClients;
+}
+
+export { createClient, getClients };
